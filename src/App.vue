f3f6e28c542a6334a3b3943bef169741a01a7205
Maksim Donic <maksimdonicc@gmail.com>
2019-12-15 00:53:17 +0100

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="title has-text-centered dividing-header">UpVote!</h2>
+    <div class="section">
+      <article v-for="submission in sortedSubmissions" :key="submission.id"   class="media" :class="{ 'blue-border': submission.votes >= 20 }">
+        <submission-component   :submission="submission"  :submissions="sortedSubmissions">
+        </submission-component>
+      </article>
+    </div> 
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import submissionComponent from './components/Submission.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+ 'submission-component': submissionComponent 
   }
+,
+ data: function() {
+    return {
+     submissions:  [
+    {
+      id: 1,
+      title: 'Yellow Pail',
+      description: 'On-demand sand castle construction expertise.',
+      url: '#',
+      votes: 16,
+      avatar: '../images/avatars/daniel.jpg',
+      submissionImage: '../images/submissions/image-aqua.png',
+    },
+    {
+      id: 2,
+      title: 'Supermajority: The Fantasy Congress League',
+      description: 'Earn points when your favorite politicians pass legislation.',
+      url: '#',
+      votes: 11,
+      avatar: '../images/avatars/kristy.png',
+      submissionImage: '../images/submissions/image-rose.png',
+    },
+    {
+      id: 3,
+      title: 'Tinfoild: Tailored tinfoil hats',
+      description: 'We have your measurements and shipping address.',
+      url: '#',
+      votes: 17,
+      avatar: '../images/avatars/veronika.jpg',
+      submissionImage: '../images/submissions/image-steel.png',
+    },
+    {
+      id: 4,
+      title: 'Haught or Naught',
+      description: 'High-minded or absent-minded? You decide.',
+      url: '#',
+      votes: 9,
+      avatar: '../images/avatars/molly.png',
+      submissionImage: '../images/submissions/image-aqua.png',
+    }
+  ]
+    }
+  },
+      computed: {
+    sortedSubmissions: function () {
+      let submissions = this.submissions;
+      return submissions.sort(function(a,b) {
+        return b.votes - a.votes;
+      });
+      }
+    }
 }
+
 </script>
 
 <style>
